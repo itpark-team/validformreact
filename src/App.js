77a255f221021a8c5fe8665534ec9daa6e8b0c40
itpark <a.m.smyslov@yandex.ru>
2023-03-27 21:44:56 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let {
+        register,
+        formState: {errors, isValid},
+        handleSubmit,
+        reset
+    } = useForm({
+        mode: "onChange"
+    });
+
+    let [productNameSymbols, setProductNameSymbols] = useState(0);
+    let [productDescriptionSymbols, setProductDescriptionSymbols] = useState(0);
+
+    const onSubmit = (data) => {
+        console.log(data);
+
+        reset();
+    }
+
+    return (
+        <div>
+            <h2>Форма добавления нового продукта</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <p>Название продукта:
+                    <input type="text" {
+                        ...register("productName", {
+                            required: "название продукта не может быть пустым",
+                            minLength: {
+                                value: 6,
+                                message: "Минимум 6 символов."
+                            },
+                            maxLength: {
+                                value: 10,
+                                message: "Максимум 10 символов."
+                            }
+                        })
+                    }
+                           />
+                    {/*onChange={(event) => setProductNameSymbols(event.target.value.length)}*/}
+                    <b>{productNameSymbols}</b>
+                </p>
+                <div>{errors?.productName?.message}</div>
+
+                <p>Описание продукта:
+                    <input type="text" {
+                        ...register("productDescription", {
+                            required: "описание продукта не может быть пустым",
+                            minLength: {
+                                value: 6,
+                                message: "Минимум 6 символов."
+                            },
+                            maxLength: {
+                                value: 15,
+                                message: "Максимум 15 символов."
+                            }
+                        })
+                    }
+                           />
+
+                    {/*onChange={(event) => setProductDescriptionSymbols(event.target.value.length)}*/}
+                    <b>{productDescriptionSymbols}</b>
+                </p>
+                <div>{errors?.productDescription?.message}</div>
+
+                <p><input type="submit" value={"Добавить"} disabled={!isValid}/></p>
+            </form>
+        </div>
+    );
+};
 
 export default App;
